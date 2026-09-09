4115eb6e239fff35247c9fa6f70ebf48dc222e16
tyl3r-44 <tylerdanebarr.official@gmail.com>
2026-09-09 15:00:12 -0500

    task 5: group by room

--- devices.py
+++ devices.py
@@ -32,3 +32,14 @@ def to_status(device):
         "online": device["online"]
     }
 print(to_status(readings[0]))
+
+def by_room(devices):
+    room_dict = {}
+    for device in devices:
+        room = device["room"]
+        if room not in room_dict:
+            room_dict[room] = []
+        room_dict[room].append(device["name"])
+    return room_dict
+
+print(by_room(readings))
